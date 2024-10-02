77bbc3bffcf2aec9c2b793ace7cd478bc9133437
Aparecida Ferreira <108874543+cidaci2000@users.noreply.github.com>
2024-10-02 20:11:15 -0300

    Add files via upload

---
+++ alessandra1/adm.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="contato.css">
+    <title>contato</title>
+</head>
+<body>
+    <header>
+        <h1>Sorveteria Delícia - administrador</h1>
+        <nav>
+            <ul>
+                <li><a href="index.php">HOME</a></li>
+              
+                
+              </ul>
+        </nav>
+       
+    </header>
+    <section class="contato">
+    <a href="listacliente.php"><button>Lista de clientes</button></a>
+    <a href="produto.php"><button>Cadastrar produto</button></a>
+    <a href="cardapio.php"><button>visualizar sorvetes</button></a>
+    <a href="colab.html"><button>Colaboradores</button></a>
+
+
+        
+                
+    </section>
+    <footer>
+        <p>&copy;2023 Sorveteria delicia.Todos os  direitos reservados.</p>
+    </footer>
+   
+</body>
+</html>
